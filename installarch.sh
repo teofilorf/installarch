5e62093c4fbab592c308e503f513fab5235478e7
Teófilo Rabêlo Filho <teofilo@rabelo.net.br>
2017-04-30 18:40:36 -0300

    update virtualbox pach

--- installarch.sh
+++ installarch.sh
@@ -8,5 +8,6 @@ yaourt --noconfirm -a -Syu google-chrome wine virtualbox vagrant yakuake mc rdes
 sudo cp winbox.desktop /usr/share/applications/
 ln -s /home/$USER/Dropbox/bin /home/$USER/
 sudo ln -s /usr/lib32/libcrypto.so.1.1 /usr/lib32/libcrypto.so.4
-
+wget -c http://download.virtualbox.org/virtualbox/5.1.22/Oracle_VM_VirtualBox_Extension_Pack-5.1.22-115126.vbox-extpack
+sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.1.22-115126.vbox-extpack
 sudo reboot
