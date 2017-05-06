c8bbe567270d5fa4e8f8d504d7179ba852f72480
teofilorf <teofilo@rabelo.net.br>
2017-05-06 12:51:56 -0300

    ajuste para manjaro

--- installarch.sh
+++ installarch.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 #instala pacotes
-yaourt --noconfirm -a -Syu google-chrome wine vboxwrapper virtualbox-host-modules-arch virtualbox-guest-iso virtualbox vagrant yakuake mc rdesktop nmap sublime-text dropbox lib32-openssl gns3-gui gns3-server iouyap gns3-launcher-icons plasma5-applets-network-monitor plasma5-applets-resources-monitor-git
+yaourt --noconfirm -Syu google-chrome wine vboxwrapper virtualbox-host-modules-arch virtualbox-guest-iso virtualbox vagrant mc rdesktop nmap sublime-text dropbox lib32-openssl iouyap dynamips gns3-gui gns3-server  gns3-launcher-icons
 
 
 #configura
 
 sudo cp winbox.desktop /usr/share/applications/
 ln -s /home/$USER/Dropbox/bin /home/$USER/
+
 sudo ln -s /usr/lib32/libcrypto.so.1.1 /usr/lib32/libcrypto.so.4
+
 wget -c http://download.virtualbox.org/virtualbox/5.1.22/Oracle_VM_VirtualBox_Extension_Pack-5.1.22-115126.vbox-extpack
+
 sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.1.22-115126.vbox-extpack
+
+echo -e '!#/bin/bash\n$PATH=$PATH:$HOME/bin' > ~/.extend.bash_profile
+
 sudo reboot
